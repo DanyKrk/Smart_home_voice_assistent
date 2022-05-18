f0801f0bfd89b13980f3f14204b4a62dd94a224a
DanyKrk <daniel.k.8576@gmail.com>
2022-05-18 14:58:39 +0200

    finished TextParser

--- Commands_translator_task/translations_JSON_generator.py
+++ Commands_translator_task/translations_JSON_generator.py
@@ -11,9 +11,10 @@ def generate_translations_JSON(home_dict):
 def generate_translations_dict():
     t_dict = {}
     t_dict["devices"] = {}
-    t_dict["places"] = {}
+    t_dict["rooms"] = {}
     t_dict["detailed_places"] = {}
     t_dict["storeys"] = {}
+    t_dict["actions"] = {}
     return t_dict
 
 def add_object(t_dict, category, name, polish_names):
@@ -23,13 +24,13 @@ def add_object(t_dict, category, name, polish_names):
     t_dict[category][name] = polish_names
 
 t_dict = generate_translations_dict()
-add_object(t_dict, "places", "kitchen", ["kuchnia", "kuchni"])
-add_object(t_dict, "places", "bedroom", ["sypialnia", "sypialni"])
-add_object(t_dict, "places", "guest room", ["pokój gościnny", "gościnny", "gościnnym", "pokoju gościnnym"])
-add_object(t_dict, "places", "workshop", ["pracownia", "pracowni"])
-add_object(t_dict, "places", "salon", ["salon", "salonie"])
-add_object(t_dict, "places", "anteroom", ["przedpokój", "przedpokoju"])
-add_object(t_dict, "places", "staircase", ["schody", "klatka schodowa", "klatce schodowej", "schodach"])
+add_object(t_dict, "rooms", "kitchen", ["kuchnia", "kuchni"])
+add_object(t_dict, "rooms", "bedroom", ["sypialnia", "sypialni"])
+add_object(t_dict, "rooms", "guest room", ["pokój gościnny", "gościnny", "gościnnym", "pokoju gościnnym"])
+add_object(t_dict, "rooms", "workshop", ["pracownia", "pracowni"])
+add_object(t_dict, "rooms", "salon", ["salon", "salonie"])
+add_object(t_dict, "rooms", "anteroom", ["przedpokój", "przedpokoju"])
+add_object(t_dict, "rooms", "staircase", ["schody", "klatka schodowa", "klatce schodowej", "schodach"])
 
 add_object(t_dict, "storeys", 1, ["parter", "parterze"])
 add_object(t_dict, "storeys", 2, ["pierwsze", "pierwsze piętro", "pierwszym", "pierwszym piętrze"])
@@ -37,5 +38,10 @@ add_object(t_dict, "storeys", 3, ["drugie", "drugie piętro", "drugim", "drugim 
 add_object(t_dict, "storeys", 0, ["piwnica", "piwnicy"])
 
 add_object(t_dict, "devices", "light", ["lampa", "lampę", "światło", "oświetlenie", "żarówka", "żarówkę"])
+
+add_object(t_dict, "actions", "on", ["włączyć", "włącz", "załączyć", "załącz", "zapalić", "zapal", "zaświecić", "zaświeć"])
+add_object(t_dict, "actions", "off", ["wyłączyć", "wyłącz", "zgasić", "zgaś"])
+add_object(t_dict, "actions", "toggle", ["przełączyć", "przełącz", "zmienić", "zmień"])
+
 
 generate_translations_JSON(t_dict)
